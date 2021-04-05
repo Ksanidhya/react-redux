a95f629c4887b552e1676b49c5a653fa3b240314
Kumar Sanidhya <kumar.sanidhya@infor.com>
2021-04-05 17:53:37 +0530

    6th commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+function Header(props)
+{
+    console.warn(props.data)
+    return(
+        <div>
+             <div className="add-to-cart">
+                <img src="https://loading.io/s/icon/4tk536.svg" />
+            </div>
+        </div>
+    )
+}
+
+export default Header
